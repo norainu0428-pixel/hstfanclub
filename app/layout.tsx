c843255dc0eea11cba5d25bdfebfd7429151b025
norainu0428-pixel <norainu0428@gmail.com>
2026-02-08 00:49:01 +0900

    sumhotaiou

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}
       >
-        {children}
+        <div className="app-content">
+          {children}
+        </div>
       </body>
     </html>
   );
